af3eb6a14b1ad1049f7f8912f4a7f437193a7c4f
Kasey Arnold <kaseyarnold15@gmail.com>
2020-04-25 22:22:10 +0000

    Automatically backed up by Learn

--- db/migrate/20200425222114_create_dogs.rb
+++ db/migrate/20200425222114_create_dogs.rb
@@ -1,7 +1,12 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
   def up
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
     end
-
-    def down
-    end
+  end
+ 
+  def down
+    drop_table :dogs
+  end
 end
